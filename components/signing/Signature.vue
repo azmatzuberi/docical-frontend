23640e45f4788cd94a4204337758d75724d09d1c
Azmat Zuberi <azmatzuberi@gmail.com>
2022-06-10 22:18:53 -0400

    Calibrated x and y coordinates for 900 and 1271

--- components/signing/Signature.vue
+++ components/signing/Signature.vue
@@ -237,36 +237,12 @@ export default {
       console.log("Factor", this.factor);
       let xCalculated;
       let yCalculated;
-
-      // Xfactor
-      // iPad Mini - X
-      if (this.factor > 1.6 && this.factor <= 1.64) {
-        xCalculated = (this.locations.x - 22) / 1.14;
-
-        //   // iPad Air 4 - X
-      } else if (this.factor > 1.64 && this.factor < 1.65) {
-        xCalculated = (this.locations.x + 10) / 1.33;
-
-        //   // iPhone 12 Pro
-      } else if (this.factor < 1.64 && this.factor > 1.3) {
-        xCalculated = this.locations.x + 63 / 0.53;
-      } else {
-        xCalculated = (this.locations.x - 20) / 2.08;
-      }
-
-      // Yfactor
-      // iPad Mini - Y
-      if (this.factor > 1.6 && this.factor <= 1.64) {
-        yCalculated = (canvasY - this.locations.y - 63) / 1.14;
-
-        //   // iPad Air 4 - Y
-      } else if (this.factor > 1.64 && this.factor < 1.65) {
-        yCalculated = (canvasY - this.locations.y - 125) / 1.33;
-        //   // iPhone 12 Pro
-      } else if (this.factor < 1.64 && this.factor > 1.3) {
-        yCalculated = (canvasY - this.locations.y - 50) / 0.53;
-      } else {
-        yCalculated = (canvasY - this.locations.y - 140) / 2.08;
+      if (window.innerWidth < 901) {
+        xCalculated = (this.locations.x + 55) / 1.78;
+        yCalculated = (canvasY - this.locations.y - 60) / 1.78;
+      } else if (window.innerWidth > 900) {
+        xCalculated = (this.locations.x - 22) / 2.08;
+        yCalculated = (canvasY - this.locations.y - 138) / 2.08;
       }
       console.log("XCalc", xCalculated);
       console.log("YCalc", yCalculated);
@@ -281,16 +257,16 @@ export default {
       console.log("Sig width", pngDims.width);
       console.log("Sig height", pngDims.height);
       let count = 0;
-      for (let i = 0; i < 11; i++) {
-        firstPage.drawImage(pngImage, {
-          x: count,
-          y: count,
-          width: pngDims.width,
-          height: pngDims.height,
-          scale: pngDims,
-        });
-        count = i * 100;
-      }
+      // for (let i = 0; i < 11; i++) {
+      //   firstPage.drawImage(pngImage, {
+      //     x: count,
+      //     y: count,
+      //     width: pngDims.width,
+      //     height: pngDims.height,
+      //     scale: pngDims,
+      //   });
+      //   count = i * 100;
+      // }
       firstPage.drawImage(pngImage, {
         x: xCalculated,
         y: yCalculated,
